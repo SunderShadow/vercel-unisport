90fabe74e83fa955598659a5cc6fc54b235c198d
EmOtIoN <sunundershadow@gmail.com>
2026-02-09 13:09:00 +0300

    Init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg'
+
+	let { children } = $props()
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Юниспорт — твой помощник в мире спорта</title>
+</svelte:head>
+
+<header></header>
+
+{@render children()}
