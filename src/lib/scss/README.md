58ca13872d37779f7a260755a7cc422a8b0864d9
EmOtIoN <sunundershadow@gmail.com>
2026-02-18 16:38:55 +0300

    Адаптирован Header

---
+++ src/lib/scss/README.md
@@ -0,0 +1,5 @@
+# Здесь содержатся стили
+
+* [mixins](./mixins) - [документация](https://sass-lang.com/documentation/at-rules/mixin/)
+  * [scr.scss](./mixins/scr.scss) - Размер экрана
+* [vars.scss](./vars.scss) - Глобальные переменные
--- src/routes/+layout/Header.svelte
+++ src/routes/+layout/Header.svelte
@@ -1,51 +1,74 @@
 <script lang="ts">
 	import logo from '$lib/assets/img/logo.png?enhanced&format=webp'
 
+	// Mobile only
+	let navVisible = $state(false)
+
 	let nav = [
 		{ href: '#', text: 'О проекте' },
 		{ href: '#', text: 'Подписка' },
 		{ href: '#', text: 'Единый абонемент' },
 		{ href: '#', text: 'Стать партнером' }
 	]
+
+	function toggleNav() {
+		navVisible = !navVisible
+	}
 </script>
 
 <header>
-	<enhanced:img src={logo} width="168" height="37" alt="Юниспорт" />
+	<button id="open-nav" class="default" aria-label="Открыть меню" onclick={toggleNav}>
+		<svg width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M4.04108 17.25C3.75496 17.25 3.5153 17.158 3.32208 16.974C3.12887 16.79 3.03193 16.5626 3.03125 16.2917C3.03058 16.0208 3.12752 15.7933 3.32208 15.6093C3.51664 15.4253 3.75631 15.3333 4.04108 15.3333H20.1982C20.4844 15.3333 20.7244 15.4253 20.9182 15.6093C21.1121 15.7933 21.2087 16.0208 21.2081 16.2917C21.2074 16.5626 21.1104 16.7903 20.9172 16.975C20.724 17.1596 20.4844 17.2513 20.1982 17.25H4.04108ZM4.04108 12.4583C3.75496 12.4583 3.5153 12.3663 3.32208 12.1823C3.12887 11.9983 3.03193 11.7709 3.03125 11.5C3.03058 11.2291 3.12752 11.0017 3.32208 10.8177C3.51664 10.6337 3.75631 10.5417 4.04108 10.5417H20.1982C20.4844 10.5417 20.7244 10.6337 20.9182 10.8177C21.1121 11.0017 21.2087 11.2291 21.2081 11.5C21.2074 11.7709 21.1104 11.9987 20.9172 12.1833C20.724 12.3679 20.4844 12.4596 20.1982 12.4583H4.04108ZM4.04108 7.66667C3.75496 7.66667 3.5153 7.57467 3.32208 7.39067C3.12887 7.20667 3.03193 6.97922 3.03125 6.70833C3.03058 6.43744 3.12752 6.21 3.32208 6.026C3.51664 5.842 3.75631 5.75 4.04108 5.75H20.1982C20.4844 5.75 20.7244 5.842 20.9182 6.026C21.1121 6.21 21.2087 6.43744 21.2081 6.70833C21.2074 6.97922 21.1104 7.20699 20.9172 7.39162C20.724 7.57626 20.4844 7.66794 20.1982 7.66667H4.04108Z" fill="#272327"/>
+		</svg>
+	</button>
 
-	<nav>
+	<div id="logo">
+		<enhanced:img src={logo} width="168" height="37" alt="Юниспорт" />
+	</div>
+
+	<nav class:visible={navVisible}>
 		{#each nav as link}
 			<a href={link.href}>{link.text}</a>
 		{/each}
 	</nav>
 
-	<div class="right">
-		<button id="city-choose">
-			<span>Ростов-на-Дону</span>
+	<button id="city-choose">
+		<span>Ростов-на-Дону</span>
 
-			<svg
-				width="20"
-				height="20"
-				viewBox="0 0 20 20"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg">
-				<path
-					d="M5 7.5L10 12.5L15 7.5"
-					stroke="#374151"
-					stroke-width="1.33333"
-					stroke-linecap="round"
-					stroke-linejoin="round" />
-			</svg>
-		</button>
+		<svg
+			width="20"
+			height="20"
+			viewBox="0 0 20 20"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M5 7.5L10 12.5L15 7.5"
+				stroke="#374151"
+				stroke-width="1.33333"
+				stroke-linecap="round"
+				stroke-linejoin="round" />
+		</svg>
+	</button>
 
-		<button>Вход</button>
-	</div>
+	<button id="auth">
+		<span>Вход</span>
+		<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M8.33203 8.41312V6.50099C8.33203 5.99387 8.53152 5.50751 8.8866 5.14892C9.24169 4.79032 9.72328 4.58887 10.2254 4.58887H20.0712C20.5734 4.58887 21.055 4.79032 21.4101 5.14892C21.7652 5.50751 21.9646 5.99387 21.9646 6.50099V17.9738C21.9646 18.4809 21.7652 18.9672 21.4101 19.3258C21.055 19.6844 20.5734 19.8859 20.0712 19.8859H10.2254C9.72328 19.8859 9.24169 19.6844 8.8866 19.3258C8.53152 18.9672 8.33203 18.4809 8.33203 17.9738V16.0616" stroke="#272327" stroke-width="1.4375" stroke-linecap="round" stroke-linejoin="round"/>
+			<path d="M12.8766 16.0621L16.6634 12.2378L12.8766 8.41357M2.27344 12.2378H15.906" stroke="#272327" stroke-width="1.4375" stroke-linecap="round" stroke-linejoin="round"/>
+		</svg>
+	</button>
 </header>
 
 <style lang="scss">
+	@use "$lib/scss/mixins/scr";
+
 	header {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
+
+		gap: 35px;
 
 		height: var(--header-height);
 		padding-left: 20px;
@@ -57,31 +80,91 @@
 
 		font-size: 0.775em;
 		font-weight: 500;
+
+		@include scr.mobile {
+      gap: 10px;
+		}
 	}
 
 	nav {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-
+		justify-self: center;
 		display: flex;
 		gap: 30px;
+    margin-left: auto;
 
-		transform: translate(-50%, -50%);
+		@media (min-width: 1101px) {
+      transform: translateX(20%);
+		}
+
+    @include scr.tablet {
+			--visible: 1;
+			--shadow-opacity: calc(.25 * (1 - var(--visible)));
+
+			flex-direction: column;
+			gap: 10px;
+
+			position: fixed;
+			top: var(--header-height);
+			height: calc(100dvh - var(--header-height));
+			bottom: 0;
+			left: 0;
+			width: 100%;
+			max-width: 70vw;
+
+      box-shadow: 0 100vw 0 100vw rgba(#000, var(--shadow-opacity));
+      background: #fff;
+      transform: translateX(calc(-100% * var(--visible)));
+			transition-property: transform, box-shadow;
+			transition-duration: var(--transition-duration);
+			z-index: 10;
+
+      &.visible {
+        --visible: 0;
+      }
+    }
 	}
 
-	.right {
-		flex-shrink: 0;
+	#logo {
+		img {
+			object-fit: contain;
+			height: 100%;
+		}
 
-		display: flex;
-		gap: 35px;
+		@include scr.tablet {
+			height: 25px;
+		}
 	}
 
-	button {
-		height: 32px;
-		width: 138px;
+	#auth {
+    height: 32px;
+    width: 138px;
+
 		padding: 0;
 		cursor: pointer;
+
+    @media (min-width: 1100px) {
+      svg {
+        display: none;
+      }
+    }
+
+		@media (max-width: 1100px) {
+      height: fit-content;
+      width: auto;
+			background: none;
+
+      span {
+        display: none;
+      }
+		}
+	}
+
+	#open-nav {
+		all: unset;
+
+    @include scr.desktop {
+      display: none;
+    }
 	}
 
 	#city-choose {
@@ -91,6 +174,12 @@
 		gap: 8px;
 		align-items: center;
 
+    margin-left: auto;
+
 		cursor: pointer;
+
+    @include scr.tablet {
+      display: none;
+    }
 	}
 </style>
