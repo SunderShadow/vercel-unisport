9dbde49ed3457fd7f556da4777592b09c293a21a
EmOtIoN <sunundershadow@gmail.com>
2026-03-05 22:31:23 +0300

    /catalog/events fix

--- src/routes/(app)/catalog/events/+page.svelte
+++ src/routes/(app)/catalog/events/+page.svelte
@@ -39,7 +39,7 @@
 							img={img2}
 							title="Первый триатлон в Ростове-на-Дорну"
 							date="27.12.2024"
-							href="/events/1" />
+							href="/catalog/events/1" />
 					</SplideSlide>
 					<SplideSlide>
 						<FastArticle
@@ -47,7 +47,7 @@
 							img={img2}
 							title="Ростовский городской зимний забег"
 							date="27.12.2024"
-							href="/events/2" />
+							href="/catalog/events/2" />
 					</SplideSlide>
 					<SplideSlide>
 						<FastArticle
@@ -55,7 +55,7 @@
 							img={img3}
 							title="Мастер-класс по глиномесенью"
 							date="27.12.2024"
-							href="/events/3" />
+							href="/catalog/events/3" />
 					</SplideSlide>
 					<SplideSlide>
 						<FastArticle
@@ -63,7 +63,7 @@
 							img={img3}
 							title="Бесплатная тренировнка по Йоге"
 							date="27.12.2024"
-							href="/events/4" />
+							href="/catalog/events/4" />
 					</SplideSlide>
 				{/each}
 			{/snippet}
@@ -79,25 +79,25 @@
 					img={img2}
 					title="Первый триатлон в Ростове-на-Дорну"
 					date="27.12.2024"
-					href="/events/1" />
+					href="/catalog/events/1" />
 				<FastArticle
 					badge="Бесплатно"
 					img={img2}
 					title="Ростовский городской зимний забег"
 					date="27.12.2024"
-					href="/events/2" />
+					href="/catalog/events/2" />
 				<FastArticle
 					badge="от 1500₽"
 					img={img3}
 					title="Мастер-класс по глиномесенью"
 					date="27.12.2024"
-					href="/events/3" />
+					href="/catalog/events/3" />
 				<FastArticle
 					badge="от 1500₽"
 					img={img3}
 					title="Бесплатная тренировнка по Йоге"
 					date="27.12.2024"
-					href="/events/4" />
+					href="/catalog/events/4" />
 			{/each}
 		</div>
 	</section>
